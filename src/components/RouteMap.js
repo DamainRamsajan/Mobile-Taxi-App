486a5af2f6334cadb9919398dd72b37523c836b1
Damain Ramsajan <dpramsajan@gmail.com>
2021-02-06 12:06:47 +0100

    React native map directions setup complete

---
+++ src/components/RouteMap.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+const RouteMap = () => {
+  const origin = {latitude: 28.450627, longitude: -16.263045};
+  const destination = {latitude: 28.460127, longitude: -16.269845};
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyAC9D2fMi7jofyo9Sw3Wd9AG47FQWTLmkY';
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={{width: '100%', height: '100%'}}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 28.450627,
+          longitude: -16.263045,
+          latitudeDelta: 0.0222,
+          longitudeDelta: 0.0121,
+        }}>
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={5}
+          strokeColor="green"
+        />
+        <Marker coordinate={origin} title={'origin'} />
+        <Marker coordinate={destination} title={'destination'} />
+      </MapView>
+    </View>
+  );
+};
+
+export default RouteMap;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 440,
+    backgroundColor: '#a5abff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/screens/SearchResults.js
+++ src/screens/SearchResults.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import HomeMap from '../components/HomeMap';
+import RouteMap from '../components/RouteMap';
 import TaxiTypes from '../components/TaxiTypes';
 
 const SearchResults = () => {
   return (
     <View>
-      <HomeMap />
+      <RouteMap />
       <TaxiTypes />
     </View>
   );
